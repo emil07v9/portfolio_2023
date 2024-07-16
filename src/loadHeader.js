614e2711783d3a2cd44d4fbb80fb9e72f1aa0187
Emilie Schultz <emil07v9@stud.kea.dk>
2024-07-16 12:37:59 +0200

    header comp

---
+++ src/loadHeader.js
@@ -0,0 +1,9 @@
+// src/loadHeader.js
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header-placeholder").innerHTML = data;
+    })
+    .catch((error) => console.error("Error loading header:", error));
+});
