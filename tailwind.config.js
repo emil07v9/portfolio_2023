f3bc79e863adc594c63835c4d0442ffaec60a28f
Emilie Schultz <emil07v9@stud.kea.dk>
2024-07-16 13:22:03 +0200

    build process

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html"], // Correct glob pattern
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {},
   },
